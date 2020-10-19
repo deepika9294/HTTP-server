0bf53d7fdc0a9d6c975270c9fa6f94cfdc4ca115
Deepika Goyal <deepika.goyal0803@gmail.com>
2020-10-19 09:46:59 +0530

    improv: GET request

--- myserver.py
+++ myserver.py
@@ -13,66 +13,126 @@ import datetime
 #link
 LINK = "./htdocs/"
 
-def handle_request(client_socket, received_message):
-    try:
-        received_message = received_message.decode('utf-8')
-    except UnicodeDecodeError:
-        print("ERROR")
-    split_message = received_message.split("\r\n")
-    if "GET" in split_message[0]:
-        print("ENTER GET")
-        if("index.html" in split_message[0] or "GET / HTTP/" in split_message[0]):
-        # if("GET / HTTP/" in split_message[0]):
+def handle_get_request(client_socket, split_message):
+    request_0 = split_message[0].split(" ")
+    #default_values    ------check if need to change
+    content_type = "text/html"
+    status_code = "200 OK"
+    #request_0[0] defines method
+    #request_0[1] defines URL
+    #request_0[2] defines version
+    print("REQUEST {}".format(request_0))
 
+    # check whether to keep or not
+    if(len(request_0) < 3):
+        status_code = "400 Bad Request"
+        #send the response here itself and return
+   
+    elif "GET" in request_0[0]:
+        print("ENTER GET")
+
+
+        if('/' == request_0[1]):  
+            #if this happen then try to find index.html
             print("ENTER /")
 
             requested_document = LINK + "index.html"
-            if os.path.exists(requested_document):
-                r_file = open(requested_document, 'r')
-                current_time = datetime.datetime.now()
-                response = "HTTP/1.1 200 OK\r\n"
-                response += ("Date: " + current_time.strftime("%A") + ", "+ current_time.strftime("%d") + " " +  current_time.strftime("%b") + " " + current_time.strftime("%Y") + " " + current_time.strftime("%X") + " GMT\n")
-                response += "Accept-Ranges: bytes\r\n"
-                document_length = os.path.getsize(requested_document)
-                response += "Content-Length: " + str(document_length) + "\r\n"
-                response += "Content-Type: text/html; charset-utf-8\r\n"
-                response += "\r\n"
-                response += r_file.read()
-                r_file.close()
-                client_socket.send(response.encode())
+            content_type = "text/html"
+
+        else:
+            requested_document = LINK + request_0[1][1:]
+            print("somethingelse requested: {}".format(requested_document))
+            #check for type of file:
+            temp_content_type = request_0[1].split(".")
+            if(len(temp_content_type) == 1):
+                status_code = "404 Not Found"
+                requested_document = LINK + "error.html"
+            elif(temp_content_type[1] == "html"):
+                content_type = "text/html"
+            elif(temp_content_type[1] == "jpg" or temp_content_type[1] == "jpeg" or temp_content_type[1] == "png"):
+                content_type = "image/" + temp_content_type[1]
+            elif(temp_content_type[1] == "mp3"):
+                content_type = "audio/mpeg"
+            elif(temp_content_type[1] == "mp4"):
+                content_type = "video" + temp_content_type[1]
+
+
+
+        if os.path.exists(requested_document):
+            status_code = "200 OK"
+            r_file = open(requested_document, 'rb')
+            current_time = datetime.datetime.now()
+            response = "HTTP/1.1 " +status_code + "\r\n"
+            response += ("Date: " + current_time.strftime("%A") + ", "+ current_time.strftime("%d") + " " +  current_time.strftime("%b") + " " + current_time.strftime("%Y") + " " + current_time.strftime("%X") + " GMT\n")
+            response += "Accept-Ranges: bytes\r\n"
+            document_length = os.path.getsize(requested_document)
+            response += "Content-Length: " + str(document_length) + "\r\n"
+            response += "Content-Type: " +content_type +"; charset-utf-8\r\n"
+            response += "\r\n"
+            # response += r_file.read(document_length)
+            if("image" in content_type or "video" in content_type or "audio" in content_type):
+                # file_data = r_file.read(document_length)
+                file_data = b""
+                b = r_file.read(1)
+                while(b != b""):
+                    file_data += b
+                    b = r_file.read(1)
 
             else:
-                print("--------------------------------")
-                response = "HTTP/1.1 400 Bad Request\r\n"
-                error_file = LINK + "error.html"
-                r_file = open(error_file, 'r')
-                document_length = os.path.getsize(error_file)
-                response += "Content-Length: " + str(document_length) + "\r\n"
-                response += "Content-Type: text/html; charset-utf-8\r\n"
-                response += "\r\n"
+                file_data = r_file.read(document_length)
+
+
+
+            r_file.close()
+            print("Ffd {}".format(response))
+            # client_socket.send(response.encode())
+
+        else:
+            status_code = "404 Not Found"
+            response = "HTTP/1.1 " + status_code +"\r\n"
+            error_file = LINK + "error.html"
+            r_file = open(error_file, 'rb')
+            document_length = os.path.getsize(error_file)
+            response += "Content-Length: " + str(document_length) + "\r\n"
+            response += "Content-Type: text/html; charset-utf-8\r\n"
+            response += "\r\n"
+            
+            # size_of_error_file = os.path.getsize()
+            # response += r_file.read(document_length)
+            file_data = r_file.read(document_length)
+
+            print(response)
+            r_file.close()
+        client_socket.send(response.encode())
+        client_socket.send(file_data)       #checkif sendfile
+
+
+    # print("DECODED_MESSAGE ::: {}".format(received_message))
+    # print("SPLIT_MESSAGE ::: {}".format(split_message))
+
                 
-                # size_of_error_file = os.path.getsize()
-                response += r_file.read()
 
-                print(response)
-                r_file.close()
-                client_socket.send(response.encode())
+    #check for version, otherwise send bad request
 
-                
-
-
-    #sending response..... shoud be in other func
     
-    print("DECODED_MESSAGE ::: {}".format(received_message))
-    print("SPLIT_MESSAGE ::: {}".format(split_message))
+    
 
 
 
 def threading(client_socket):
     #decide the size
     received_message = client_socket.recv(2046)  
-    handle_request(client_socket, received_message)
-    
+    try:
+        received_message = received_message.decode('utf-8')
+        # print("STUDY {}".format(received_message))
+    except UnicodeDecodeError:
+        print("ERROR")
+    split_message = received_message.split("\r\n")
+    #handle in more better way
+    if("GET" in split_message[0]):
+        handle_get_request(client_socket, split_message)
+    else:
+        print("something else")
     client_socket.shutdown(SHUT_WR)
 
 def create_server(port):
