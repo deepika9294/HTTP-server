56d8dea8db3f6ae861c9c3eb009d88aa58965994
Deepika Goyal <deepika.goyal0803@gmail.com>
2020-10-25 08:37:55 +0530

    Added bad request in get, added location in put, added logger

--- myserver.py
+++ myserver.py
@@ -8,12 +8,16 @@ import datetime
 import json
 from urllib.parse import *	 # for parsing URL/URI
 import uuid
+import logging
 
 
 #link
 LINK = "./htdocs/"
 RESOURCES = LINK + "resources/"
 SIZE = 1024
+LOGGING = LINK + "data.log"
+logging.basicConfig(filename = LOGGING, level = logging.INFO, format = '%(asctime)s:%(filename)s:%(message)s')
+
 
 def check_version(version):
     print(version)
@@ -34,7 +38,7 @@ def get_common_response(status_code,content_type,content_length,location):
         response += "Content-Length: " + content_length + "\r\n"
 
     if(location):
-        response += "Location: " + location + "\r\n"
+        response += "Content-Location: " + location + "\r\n"
 
     return response
 
@@ -93,7 +97,7 @@ def handle_old_put_request(client_socket, message):
         status_code = "404 Not Found"
 
     content_length = None
-    location = None
+    location = file_name
     response = get_common_response(status_code,content_type,content_length,location) 
     response += "\r\n"
     client_socket.send(response.encode())
@@ -134,6 +138,7 @@ def handle_put_request(client_socket, message):
             r_file.write(parse_urlencoded(message[1]))
             r_file.close()
             status_code = "200 Ok"
+            location = url_path
 
         elif(os.path.isdir(url_path)):
             put_uuid = str(uuid.uuid4())
@@ -142,13 +147,14 @@ def handle_put_request(client_socket, message):
             r_file.write(parse_urlencoded(message[1]))
             r_file.close()
             status_code = "201 Created"
+            location = put_path
+
 
              
         else:
             status_code = "404 Not Found"
 
         content_length = None
-        location = None
         response = get_common_response(status_code,content_type,content_length,location)
         response += "\r\n"
         client_socket.send(response.encode())
@@ -159,6 +165,8 @@ def handle_put_request(client_socket, message):
             r_file.write(message[1])
             r_file.close()
             status_code = "200 Ok"
+            location = url_path
+
 
         elif(os.path.isdir(url_path)):
             put_uuid = str(uuid.uuid4())
@@ -167,11 +175,12 @@ def handle_put_request(client_socket, message):
             r_file.write(message[1])
             r_file.close()
             status_code = "201 Created"
+            location = put_path
+
         else: 
             status_code = "404 Not Found"
 
         content_length = None
-        location = None
         response = get_common_response(status_code,content_type,content_length,location)
         response += "\r\n"
         client_socket.send(response.encode())
@@ -280,7 +289,8 @@ def handle_post_request(client_socket, message):
         client_socket.send(response.encode())
     
 #handle directory case
-def handle_get_head_request(client_socket, split_message):
+def handle_get_head_request(client_socket, message):
+    split_message = message[0].split("\r\n")
     request_0 = split_message[0].split(" ")
     #default_values    ------check if need to change
     content_type = "text/html"
@@ -290,8 +300,27 @@ def handle_get_head_request(client_socket, split_message):
     request_0[1] defines URL
     request_0[2] defines version
     print("REQUEST {}".format(request_0))'''
-
+    # print(message)
+    # print(len(message[1]))
     # check condition of bad request and version 
+    if(len(message[1]) != 0):
+        status_code = "400 Bad Request"
+        file_name = LINK + "badrequest.html"
+        content_type = "text/html"
+        r_file = open(file_name, 'rb')
+        document_length = os.path.getsize(file_name)
+        location = None
+        response = get_common_response(status_code,content_type,str(document_length),location)
+ 
+        response += "\r\n"
+        file_data = r_file.read(document_length)
+        r_file.close()
+        print(response)
+        client_socket.send(response.encode())
+        if("GET" == request_0[0]):
+            client_socket.send(file_data)
+        return
+
 
     if('/' == request_0[1]):  
         #if this happen then try to find index.html
@@ -348,14 +377,18 @@ def handle_get_head_request(client_socket, split_message):
         r_file.close()
     # print(response)
     client_socket.send(response.encode())
+    logging.info('	{}	{}\n'.format(split_message[0], status_code))
+
     if("GET" == request_0[0]):
+        #resolve the broken pipe issue
         client_socket.send(file_data)       
 
 
-def threading(client_socket):
+def threading(client_socket,client_address):
     # while(True):
     #decide the size
     received_message = client_socket.recv(SIZE)
+    w = open(LOGGING, "a")
     # print("REC {}".format(received_message))  
     try:
         received_message = received_message.decode('utf-8')
@@ -372,11 +405,15 @@ def threading(client_socket):
         print("ERROR")
     split_message = message[0].split("\r\n")
     # print("MESA000 {}".format(message))
+    # log.write(client_address)
+    # log.write(message[0])
+    w.close()
+
 
     #note: sending split_message in get , as we dont require entity here
     # handle directory here
     if("GET" in split_message[0] or "HEAD" in split_message[0]):
-        handle_get_head_request(client_socket, split_message)
+        handle_get_head_request(client_socket, message)
     elif("POST" in split_message[0]):
         handle_post_request(client_socket, message)
     elif("PUT" in split_message[0]):
@@ -400,7 +437,7 @@ def create_server(port):
             client_socket, client_address = server_socket.accept()
             # print("clientSocket {} and client address {}".format(client_socket,client_address))
 
-            start_new_thread(threading,(client_socket,)) 
+            start_new_thread(threading,(client_socket,client_address,)) 
         server_socket.close()    #check whether hoga ya nhi
         
     except KeyboardInterrupt:
