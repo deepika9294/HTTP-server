1133130e5c968ab6166b1077ceb52d7c083beb75
Deepika Goyal <deepika.goyal0803@gmail.com>
2020-11-10 22:01:16 +0530

    improv: config file

--- config.py
+++ config.py
@@ -1,12 +1,37 @@
 
+'''
+Version of HTTP server  
+'''
+VERSION = '1.1'
+
+''' 
+Data size to receive from client at start
+'''
 SIZE = 4096
+
+
+''' 
+Link is root directory 
+'''
 LINK = "./htdocs/"
-favicon = '/favicon.ico'
+
+''' 
+Resources is a directory , where you dump all the post data
+'''
 RESOURCES = LINK + "resources/"
+
+'''
+log file to log different deatils of request
+'''
+LOGGING = LINK + "data.log"
+
+'''
+Username and Password for authorisation to perform delete request method
+'''
 USERNAME = 'deepika'
 PASSWORD = 'root'
-VERSION = '1.1'
-LOGGING = LINK + "data.log"
+
+
 
 
 
